e9af1e5944c000cf10305e09b0977dc82e6b62b1
Tiago Figueiredo <tcfigueiredo07@gmail.com>
2023-03-15 18:09:37 +0000

    Update Workflow-basics.R

--- Workflow-basics.R
+++ Workflow-basics.R
@@ -1,12 +1,12 @@
 # Exercises 4.4 ==========================================================================================================================================================
 
-#Why does this code not work?
+# Q1. Why does this code not work?
 my_variable <- 10
 my_varıable
 #> Error in eval(expr, envir, enclos): object 'my_varıable' not found
-#Look carefully! (This may seem like an exercise in pointlessness, but training your brain to notice even the tiniest difference will pay off when programming.)
+# A. The code does not work because the variable is not well written!
 
-#Tweak each of the following R commands so that they run correctly:
+# Q2. Tweak each of the following R commands so that they run correctly:
 
 library(tidyverse)
 
@@ -15,4 +15,14 @@ ggplot(dota = mpg) +
 
 fliter(mpg, cyl = 8)
 filter(diamond, carat > 3)
-Press Alt + Shift + K. What happens? How can you get to the same place using the menus?
+# A. Correction to the execise
+library(tidyverse)
+
+ggplot(data = mpg) +
+    geom_point(mapping = aes(x = displ, y = hwy))
+
+filter(mpg, cyl == 8)
+filter(diamonds, carat > 3)
+
+# Q3. Press Alt + Shift + K. What happens? How can you get to the same place using the menus?
+# A. Shortkeys!!
